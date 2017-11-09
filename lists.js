6ac9b2f0f8f6db79190f37b0f134c5bad7778f48
Joshua <joshua.collado022@gmail.com>
2017-11-09 01:38:12 -0500

    Figured out how to use the API to put an image of hulk in the inner HTML

--- lists.js
+++ lists.js
@@ -6,36 +6,56 @@ document.addEventListener("DOMContentLoaded", open);
 function open(){
 var publicKey =  '3e90d0fc4165b08b58fe7aaa12896014';
 var privateKey = 'd91f11281056d2227753ea91699a32385f12b42d';
-var marvelAPI = 'https://gateway.marvel.com/v1/public/comics';
+var marvelAPI = 'https://gateway.marvel.com/v1/public';
+var comicAPI = marvelAPI + '/comics';
+var characterAPI = marvelAPI + '/characters';
+var characterName = 'hulk';
+
+
+
 var xhttp = new XMLHttpRequest();
-
 var ts = new Date().getTime();
+//
+// xhttp.onload = function() {
+//   var response = JSON.parse(this.responseText);
+//   var results = response.data.results;
+//   var resultsLen = results.length;
+//   var output = '<ul>';
+//
+//   for(var i=0; i<resultsLen; i++){
+//     if(results[i].images.length > 0) {
+//       var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
+//       output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
+//     }
+//   }
+//
+//   output += '</ul>'
+//   document.getElementById('results').innerHTML=output;
+//
+// }
 
-
-xhttp.onload = function() {
+xhttp.onload = function(){
   var response = JSON.parse(this.responseText);
-  var results = response.data.results;
-  var resultsLen = results.length;
-  var output = '<ul>';
-
-  for(var i=0; i<resultsLen; i++){
-    if(results[i].images.length > 0) {
-      var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
-      output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
-    }
-  }
-  output += '</ul>'
-  document.getElementById('results').innerHTML=output;
+  var pre = response.data.results[0].thumbnail;
+  var result = pre.path + '/portrait_incredible.' + pre.extension;
+  var image = '<img src=' + result + '>'
+  document.getElementById('hulk_image').innerHTML=image;
 }
 
+var characAPI= characterAPI + '?' +
+  'hash=' + CryptoJS.MD5(ts + privateKey + publicKey).toString() + '&' +
+  'apikey=' + publicKey + '&' +
+  'ts=' + ts;
 
 
-var apiRequest = marvelAPI + '?' +
+var apiRequest = comicAPI + '?' +
       'hash=' +  CryptoJS.MD5(ts+ privateKey + publicKey).toString() + '&' +
       'apikey=' + publicKey + '&' +
       'ts=' + ts;
 
-  xhttp.open("GET", apiRequest, true);
+  var characterRequest = characAPI + '&nameStartsWith=' + characterName;
+  //xhttp.open("GET", apiRequest, true);
+  xhttp.open("GET", characterRequest, true);
   xhttp.setRequestHeader('Content-Type', 'application/json');
   xhttp.send();
 }
