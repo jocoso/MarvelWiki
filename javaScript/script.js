52a3d1760b589bac70604eda42483f4b6ad2b1c3
jocoso <joshua.collado022@gmail.com>
2017-11-19 08:25:23 -0500

    Added a menu bar

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,67 +1,15 @@
-
-
-document.addEventListener("DOMContentLoaded", open);
-
-
-function open(){
-var publicKey =  '3e90d0fc4165b08b58fe7aaa12896014';
-var privateKey = 'd91f11281056d2227753ea91699a32385f12b42d';
-var marvelAPI = 'https://gateway.marvel.com/v1/public';
-var comicAPI = marvelAPI + '/comics';
-var characterAPI = marvelAPI + '/characters';
-var characterName = 'hulk';
-
-
-
-var xhttp = new XMLHttpRequest();
-var ts = new Date().getTime();
-//
-// xhttp.onload = function() {
-//   var response = JSON.parse(this.responseText);
-//   var results = response.data.results;
-//   var resultsLen = results.length;
-//   var output = '<ul>';
-//
-//   for(var i=0; i<resultsLen; i++){
-//     if(results[i].images.length > 0) {
-//       var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
-//       output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
-//     }
-//   }
-//
-//   output += '</ul>'
-//   document.getElementById('results').innerHTML=output;
-//
-// }
-
-xhttp.onload = function(){
-  var response = JSON.parse(this.responseText);
-  var pre = response.data.results[0].thumbnail;
-  var imgResult = pre.path + '/portrait_incredible.' + pre.extension;
-  var titleResult = response.data.results[0].name;
-  var descResult = response.data.results[0].description;
-  var image = '<img src=' + imgResult + '>'
-  
-  document.getElementById('hulk_title').innerHTML=titleResult;
-  document.getElementById('hulk_image').innerHTML=image;
-  document.getElementById('hulk_description').innerHTML=descResult;
-
+function changeButtonName(buttonName, content){
+  document.getElementById(buttonName).innerHTML=content;
 }
 
-var characAPI= characterAPI + '?' +
-  'hash=' + CryptoJS.MD5(ts + privateKey + publicKey).toString() + '&' +
-  'apikey=' + publicKey + '&' +
-  'ts=' + ts;
+window.onload = function(){
+var allLinks = document.getElementById('search-type').getElementsByTagName('a');
 
+for(var i = 0; i < allLinks.length; i++){
+  allLinks[i].addEventListener('click', redirect, false);
+}
+}
 
-var apiRequest = comicAPI + '?' +
-      'hash=' +  CryptoJS.MD5(ts+ privateKey + publicKey).toString() + '&' +
-      'apikey=' + publicKey + '&' +
-      'ts=' + ts;
-
-  var characterRequest = characAPI + '&nameStartsWith=' + characterName;
-  //xhttp.open("GET", apiRequest, true);
-  xhttp.open("GET", characterRequest, true);
-  xhttp.setRequestHeader('Content-Type', 'application/json');
-  xhttp.send();
+function redirect(ev){
+  ev.preventDefault();
 }
